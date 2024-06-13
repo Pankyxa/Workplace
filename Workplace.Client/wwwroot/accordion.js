c49ecf2277bb680abaffe2e07280e80ac5f3acb9
AleksRiffel <avr-tum@mail.ru>
2024-06-13 19:57:07 +0500

    Design of Student TaskList

---
+++ Workplace.Client/wwwroot/accordion.js
@@ -0,0 +1,13 @@
+﻿window.activateBootstrapComponents = function () {
+    var collapaseElements = document.querySelectorAll('.collapse');
+    var accordionButtons = document.querySelectorAll('.accordion-button');
+
+    collapaseElements.forEach(function (element, index) {
+        var accordionButton = accordionButtons[index];
+        accordionButton.addEventListener('click', function () {
+            var target = accordionButton.getAttribute('data-bs-target');
+            var targetElement = document.querySelector(target);
+            targetElement.classList.toggle('show');
+        });
+    });
+};
